aca883fb7022909b5cfb583397ca3166ca797ce6
Xiao Luo <xluo4416@gmail.com>
2025-10-11 16:06:38 +1100

    feat: Add Exercise model and DbContext

---
+++ FitTrackPro/Models/Exercise.cs
@@ -0,0 +1,23 @@
+// FitTrackPro/Models/Exercise.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace FitTrackPro.Models
+{
+    public class Exercise
+    {
+        [Key]  // This represents the primary key in the database
+        public int Id { get; set; }
+
+        [Required] // This indicates that the field is required
+        public string Name { get; set; }
+
+        public string MuscleGroup { get; set; }  // Target muscle group
+
+        public string Difficulty { get; set; } // Difficutly 
+
+        public string Equipment { get; set; } // Required equipment
+
+        public string Instructions { get; set; }  // Instructions
+
+    }
+}
