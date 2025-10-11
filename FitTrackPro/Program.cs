43a08cf416650271b44ed64012bcfd083f79dd60
Xiao Luo <xluo4416@gmail.com>
2025-10-11 16:53:54 +1100

    Submit program.cs

--- FitTrackPro/Program.cs
+++ FitTrackPro/Program.cs
@@ -1,7 +1,18 @@
+
+using FitTrackPro.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+
+builder.Services.AddDbContext<FitTrackPro.Data.ApplicationDbContext>(options =>
+options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 var app = builder.Build();
 
